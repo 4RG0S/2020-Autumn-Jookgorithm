7774c9dd8d5b3635222153f92f794f3ff97fd198
mjkim <hjn966@naver.com>
2020-10-06 21:37:39 +0900

    1064 Solved Silver 5 www.acmicpc.net/problem/1064

--- 김민재/[20.10.06]1064.py
+++ 김민재/[20.10.06]1064.py
@@ -4,7 +4,6 @@ from sys import stdin
 def get_distance(pos1, pos2):
     return ((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2) ** 0.5
 
-
 xA, yA, xB, yB, xC, yC = tuple(map(int, stdin.readline().split()))
 A, B, C = (xA, yA), (xB, yB), (xC, yC)
 
