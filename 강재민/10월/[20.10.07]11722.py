16d5aabdaecf5ae8b0fe9c3a878d6ca3f99f7422
kylin-1q84 <shadekj@naver.com>
2020-10-07 14:43:46 +0900

    [20.10.07]11722.py solved by kylin

---
+++ 강재민/10월/[20.10.07]11722.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+N = int(input())
+A = list( map( int , input().split() ))
+S = [ 0 ] * N
+for idx, item in enumerate(A):
+    minnum = 0
+    for j in range( 0, idx ):
+        if( item < A[j] ):
+            if( minnum < S[j] ):
+                minnum = S[j]
+    S[idx] = minnum + 1
+print( max(S) )
+
+            
